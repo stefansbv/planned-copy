ad9786ca4d56341e2a97980447ff58ad64e7dab6
Stefan Suciu <stefan@s2i2.ro>
2015-01-23 14:07:19 +0200

    Use Exceptions module.

--- lib/App/ConfigManager/Resource/Read.pm
+++ lib/App/ConfigManager/Resource/Read.pm
@@ -11,6 +11,8 @@ use namespace::autoclean;
 use YAML::Tiny 1.57;                         # errstr deprecated
 use Try::Tiny;
 
+use App::ConfigManager::Exceptions;
+
 has 'resource_file' => (
     is     => 'ro',
     isa    => Path,
