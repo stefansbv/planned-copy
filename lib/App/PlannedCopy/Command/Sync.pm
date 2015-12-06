fb61f2865dc31be8753e90b1380a055b6acd0df5
Stefan Suciu <stefan@s2i2.ro>
2015-12-06 16:00:15 +0200

    Add optional positional param for sync subcommand.
    Skips all other but named (destination) file.

--- lib/App/PlannedCopy/Command/Sync.pm
+++ lib/App/PlannedCopy/Command/Sync.pm
@@ -25,12 +25,20 @@ parameter 'project' => (
     documentation => q[Project name.],
 );
 
+parameter 'dst_name' => (
+    is            => 'rw',
+    isa           => 'Str',
+    required      => 0,
+    documentation => q[Optional destination file name.],
+);
+
 sub execute {
     my ( $self ) = @_;
 
     my $file = $self->config->resource_file( $self->project );
     my $res  = App::PlannedCopy::Resource->new( resource_file => $file);
     my $iter = $res->resource_iter;
+    my $name = $self->dst_name;
 
     say 'Job: ', $res->count, ' file', ( $res->count != 1 ? 's' : '' ),
         ' to check and synchronize', ( $self->verbose ? ' (verbose)' : '' ),
@@ -41,6 +49,11 @@ sub execute {
     while ( $iter->has_next ) {
         $self->set_error_level('info');
         my $rec  = $iter->next;
+        if ($name) {
+
+            # Skip until found; not efficient but simple to implement ;)
+            next unless $rec->dst->_name eq $name;
+        }
         my $cont = try { $self->validate_element($rec) }
         catch {
             my $e = $self->handle_exception($_);
