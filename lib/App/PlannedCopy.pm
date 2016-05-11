8cafc4e7e074a93d20a58bc503a1dddeefb7215d
Stefan Suciu <stefbv70@gmail.com>
2016-05-11 10:43:21 +0300

    Use the Counter role.

--- lib/App/PlannedCopy.pm
+++ lib/App/PlannedCopy.pm
@@ -12,7 +12,7 @@ use MooseX::App qw(Color Version);
 use App::PlannedCopy::Config;
 use App::PlannedCopy::Issue;
 
-with qw(App::PlannedCopy::Role::Base);
+with qw(App::PlannedCopy::Role::Counters);
 
 app_namespace 'App::PlannedCopy::Command';
 
