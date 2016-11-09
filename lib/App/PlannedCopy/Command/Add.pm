5f9a0598449a3f7ca719974cb9f2a49b46818a9f
Stefan Suciu <stefbv70@gmail.com>
2016-11-09 13:47:55 +0200

    Start implementing the add command.

---
+++ lib/App/PlannedCopy/Command/Add.pm
@@ -0,0 +1,188 @@
+package App::PlannedCopy::Command::Add;
+
+# ABSTRACT: Add file(s) to a project and update the resource file
+
+use 5.010001;
+use utf8;
+use MooseX::App::Command;
+use Try::Tiny;
+use Path::Tiny;
+use App::PlannedCopy::Ls;
+use namespace::autoclean;
+
+extends qw(App::PlannedCopy);
+
+use constant RESOURCE_FILE => 'resource.yml';
+
+with qw(App::PlannedCopy::Role::Utils
+        App::PlannedCopy::Role::Printable);
+
+parameter 'project' => (
+    is            => 'rw',
+    isa           => 'Str',
+    required      => 1,
+    documentation => q[Project name.],
+);
+
+parameter 'files' => (
+    is            => 'rw',
+    isa           => 'Str',
+    required      => 1,
+    documentation => q[The file(s) to be added to the project.],
+);
+
+option 'update' => (
+    is            => 'rw',
+    isa           => 'Bool',
+    cmd_aliases   => [qw(u)],
+    documentation => q[Remove entries for nonexistent files.],
+);
+
+option 'interactive' => (
+    is            => 'rw',
+    isa           => 'Bool',
+    cmd_aliases   => [qw(i)],
+    documentation => q[Enter interactive mode.],
+);
+
+sub run {
+    my ( $self ) = @_;
+
+    $self->check_project_name;
+
+    my $path_param = $self->files;
+
+    my $file_list = [];
+
+    # The parameter has wildcards?
+    if ( $path_param =~ m{[?*]} ) {
+        say "param has wildcards!";
+
+        my ( $file_path, $file_name );
+        if ( $path_param =~ m{^(.*)/([^/]*)$} ) {
+            $file_path = $1;
+            $file_name = $2;
+        }
+        die "Widcards are allowed only for the file name!\n"
+            if $file_path =~ m{[?*]};
+
+        say 'file_path: ', $file_path;
+        say 'file_name: ', $file_name;
+
+        # Go and build the file list
+        $file_list = $self->gather_files($file_path, $file_name);
+    }
+    else {
+        say "param has NO wildcards!";
+
+        say "is file: ", path($path_param)->is_file ? 'yes' : 'no';
+        say "is dir:  ", path($path_param)->is_dir  ? 'yes' : 'no';
+
+        if ( path($path_param)->is_file ) {
+            my ( $file_path, $file_name );
+            if ( $path_param =~ m{^(.*)/([^/]*)$} ) {
+                $file_path = $1;
+                $file_name = $2;
+            }
+            else {
+                $file_path = '.';
+                $file_name = $path_param;
+            }
+
+            die "The '$file_path' path does not exists!\n"
+                unless path($file_path)->is_dir;
+
+            say 'file_path: ', $file_path;
+            say 'file_name: ', $file_name;
+
+            # Add the file
+            push @{$file_list}, $path_param;
+        }
+        if ( path($path_param)->is_dir ) {
+
+            # Go and gather all files
+            $file_list = $self->gather_files($path_param);
+        }
+    }
+
+    use Data::Dump; dd $file_list;
+
+    $self->print_summary;
+
+    return;
+}
+
+sub gather_files {
+    my ( $self, $path, $wildcard ) = @_;
+
+    die "The path was not provided for 'gather_files'!\n"
+        unless $path;
+
+    my $rule = Path::Iterator::Rule->new;
+    $rule->skip_vcs;
+    $rule->skip(
+        $rule->new->file->empty,
+        $rule->new->file->name(RESOURCE_FILE),
+    );
+    $rule->name($wildcard) if $wildcard;
+    $rule->min_depth(1);
+
+    my $next = $rule->iter( $path,
+        { relative => 0, sorted => 1, follow_symlinks => 0 } );
+    my $dirs = [];
+    while ( defined( my $item = $next->() ) ) {
+        my $file = path $item;
+        push @{$dirs}, $file->relative($path)->stringify if $file->is_file;
+    }
+    return $dirs;
+}
+
+sub print_summary {
+    my $self = shift;
+    say '';
+    say 'Summary:';
+    say ' - directories: ', $self->count_dirs;
+    say '';
+    return;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 Description
+
+Add file(s) to a project and update the resource file.
+
+plcp add <project> ./path/to/a/file  - add the 'file' file
+
+plcp add <project> ./path/           - add a directory recursively
+
+plcp add <project> ./path/*.conf     - add files using wildcards
+
+plcp add <project> file1 file2 file3 - add a list of files
+
+=head1 Interface
+
+=head2 Attributes
+
+=head3 project
+
+Required parameter attribute for the list command.  The name of the
+project - a directory name under C<repo_path>.
+
+=head2 Instance Methods
+
+=head3 run
+
+The method to be called when the C<add> command is run.
+
+=head3 print_summary
+
+Prints the summary of the command.
+
+=cut
