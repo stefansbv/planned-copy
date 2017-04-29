d0ff7a7f295fe2dbf4624c5cee537a8a76ae36a2
Stefan Suciu <stefbv70@gmail.com>
2017-04-29 08:38:48 +0300

    Add attribs to TestCmd.

--- t/lib/TestCmd.pm
+++ t/lib/TestCmd.pm
@@ -6,8 +6,25 @@ use namespace::autoclean;
 extends qw(App::PlannedCopy);
 
 with qw( App::PlannedCopy::Role::Printable
-		 App::PlannedCopy::Role::Resource::Utils
+         App::PlannedCopy::Role::Resource::Utils
          App::PlannedCopy::Role::Utils );
+
+has 'project' => (
+    is            => 'rw',
+    isa           => 'Str',
+    required      => 1,
+    documentation => q[Project name.],
+);
+
+has 'resource_file' => (
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        return $self->config->resource_file( $self->project );
+    },
+);
 
 __PACKAGE__->meta->make_immutable;
 
