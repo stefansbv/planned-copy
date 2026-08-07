f0597ba474bc30d2f8469882bb287bc0ed86112b
Stefan Suciu <stefbv70@gmail.com>
2026-08-07 14:43:14 +0300

    Silence the sftp errors; rename remote_host to _host; add POD

--- lib/App/PlannedCopy/Role/Remote.pm
+++ lib/App/PlannedCopy/Role/Remote.pm
@@ -6,9 +6,10 @@ use 5.0100;
 use utf8;
 use Moose::Role;
 use Path::Tiny;
+use Capture::Tiny ':all';
 use Net::SFTP::Foreign;
 
-has 'remote_host' => (
+has '_host' => (
     is      => 'ro',
     isa     => 'Str',
     lazy    => 1,
@@ -25,18 +26,24 @@ has 'sftp' => (
     lazy    => 1,
     default => sub {
         my $self = shift;
-        my $host = $self->remote_host // 'localhost';
+        my $host = $self->_host;
         return if $host eq 'localhost';
         my $para = [];
         push @{$para}, '-v' if $self->debug;
-        my $sftp = Net::SFTP::Foreign->new(
-            $host,
-            more => $para,
-        );
+        my $sftp;
+
+        # silence sftp
+        my ( undef, undef, undef ) = capture {
+            $sftp = Net::SFTP::Foreign->new(
+                $host,
+                more => $para,
+            );
+        };
         say "[sftp] Connecting as ", $ENV{USER} if $self->verbose;
         $sftp->error
-            and die "Unable to establish SFTP connection: " . $sftp->error . "\n";
-        # say "[sftp] status: ", $sftp->status;
+            and die "Unable to establish SFTP connection!\n     "
+            . $sftp->error . "\n";
+
         $sftp->setcwd('/') or die "Unable to change cwd: " . $sftp->error . "\n";
         say "[sftp] CWD is ", $sftp->cwd if $self->verbose;
 
@@ -47,3 +54,28 @@ has 'sftp' => (
 no Moose::Role;
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+A role that uses C<Net::SFTP::Foreign> to establish  a remote connection to a box.
+
+=head2 ATTRIBUTES
+
+=head3 _host
+
+The host name is the host option on the command line if provided or
+the host attribute from the resource file.
+
+=head3 sftp
+
+An attribute that holds an instance of C<Net::SFTP::Foreign>.
+
+=head2 INSTANCE METHODS
+
+=cut
